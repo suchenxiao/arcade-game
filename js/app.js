545f137c4439c30f05473bff720f6d3829947955
suchenxiao <suchenxiao@foxmail.com>
2018-02-20 19:12:30 +0800

    Feature: 增加对象（敌人/玩家）初始位置

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
+var Enemy = function(x, y) {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
 
     // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
+	this.x = x;
+	this.y = y;
 };
 
 // 此为游戏必须的函数，用来更新敌人的位置
@@ -21,10 +23,12 @@ Enemy.prototype.render = function() {
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
-var Player = function() {
+var Player = function(x, y) {
 
     // 玩家的图片
     this.sprite = 'images/char-boy.png';
+	this.x = x;
+	this.y = y;
 }
 Player.prototype.update = function(dt) {
 
@@ -39,13 +43,13 @@ Player.prototype.handleInput = function() {
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
-var enemy3 = new Enemy();
-var enemy4 = new Enemy();
+var enemy1 = new Enemy(101*-0.5, 83*1-15);
+var enemy2 = new Enemy(101*-0.5, 83*2-15);
+var enemy3 = new Enemy(101*-0.5, 83*3-15);
+var enemy4 = new Enemy(101*-0.5, 83*4-15);
 var allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
-var player = new Player();
+var player = new Player(101*2, 83*5-15);
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Player.handleInput()
 // 方法里面。你不需要再更改这段代码了。
